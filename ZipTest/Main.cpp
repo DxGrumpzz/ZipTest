207822eb25732680ffacf405f51757e26d7a8a1d
Josef Lintz <yosi1.lints@gmail.com>
2020-05-27 12:52:50 +0300

    * Finally managed to find the Central directory

--- ZipTest/Main.cpp
+++ ZipTest/Main.cpp
@@ -2,17 +2,28 @@
 #include <filesystem>
 #include <iostream>
 #include <string>
-
+#include <assert.h>
+#include <cmath>
 
 typedef uint8_t Byte;
 
-constexpr int PK_SIGNATURE = 0x504b0304;
+constexpr int PK_FILE_HEADER_SIGNATURE = 0x504b0304;
 
-enum class CompressionMethod
+constexpr int PK_CENTRAL_DIRECTORY_SIGNATURE = 0x504b0102;
+
+// Little endian version of the central directory signature
+constexpr int PK_CENTRAL_DIRECTORY_SIGNATURE_LITTLE_ENDIAN = 0x2014B50;
+
+
+constexpr int PK_END_OF_CENTRAL_DIRECTORY = 0x504b0506;
+
+
+enum class CompressionMethod : short
 {
     None = 0,
 
     Shrunk = 1,
+    LZW = 1,
 
     ReducedWithCompressionFactor1 = 2,
     ReducedWithCompressionFactor2 = 3,
@@ -46,6 +57,7 @@ enum class CompressionMethod
 
     PPMd_Version_I_Rev_1 = 98,
 };
+
 
 const wchar_t* GetLocalZipFileName(std::vector<Byte>& const zipFileData)
 {
@@ -96,12 +108,94 @@ void GetLocalZipFileDataW2(const std::vector<Byte>& zipFileData, wchar_t*& outDa
 };
 
 
+void GetLocalZipFileData3(const std::vector<Byte>& zipFileData, Byte*& outData, int& outdataLength)
+{
+    const short filenameLength = zipFileData[26];
+
+    const size_t startOfDataIndex = filenameLength + 30;
+
+    outdataLength = zipFileData.size() - startOfDataIndex;
+
+    outData = new Byte[outdataLength + 1] { 0 };
+
+    for (size_t a = startOfDataIndex; a < zipFileData.size(); a++)
+    {
+        Byte dataPointer = zipFileData[a];
+        outData[a - startOfDataIndex] = dataPointer;
+    };
+};
+
+
+
+void GetCentralDirectory(const uint8_t* zipFileData, const int& zipFileDataLength, std::vector<Byte>& outdata)
+{
+    int indexer = zipFileDataLength;
+    int dataIndexer = indexer - 4;
+
+    int pkSignature = 0;
+
+    while (indexer > 0)
+    {
+        pkSignature = zipFileData[dataIndexer];
+        pkSignature <<= 8;
+        pkSignature |= zipFileData[dataIndexer + 1];
+        pkSignature <<= 8;
+        pkSignature |= zipFileData[dataIndexer + 2];
+        pkSignature <<= 8;
+        pkSignature |= zipFileData[dataIndexer + 3];
+
+        outdata.insert(outdata.begin(), zipFileData[indexer - 1]);
+
+        if (pkSignature == PK_CENTRAL_DIRECTORY_SIGNATURE)
+        {
+            return;
+        };
+
+        dataIndexer--;
+        indexer--;
+    };
+};
+
+
+void GetEndCentralDirectory(const uint8_t* zipFileData, const int& zipFileDataLength, std::vector<Byte>& outdata)
+{
+    int indexer = zipFileDataLength;
+    int dataIndexer = indexer - 4;
+
+    int pkSignature = 0;
+
+    while (indexer > 0)
+    {
+        pkSignature = zipFileData[dataIndexer];
+        pkSignature <<= 8;
+        pkSignature |= zipFileData[dataIndexer + 1];
+        pkSignature <<= 8;
+        pkSignature |= zipFileData[dataIndexer + 2];
+        pkSignature <<= 8;
+        pkSignature |= zipFileData[dataIndexer + 3];
+
+        outdata.insert(outdata.begin(), zipFileData[indexer - 1]);
+
+        if (pkSignature == PK_END_OF_CENTRAL_DIRECTORY)
+        {
+            outdata.insert(outdata.begin(), zipFileData[indexer - 2]);
+            outdata.insert(outdata.begin(), zipFileData[indexer - 3]);
+            outdata.insert(outdata.begin(), zipFileData[indexer - 4]);
+
+            return;
+        };
+
+        dataIndexer--;
+        indexer--;
+    };
+};
+
 
 int main()
 {
     const wchar_t* zipFilepath = L"TestZip1.zip";
 
-    std::wifstream fileStream(zipFilepath);
+    std::ifstream fileStream(zipFilepath, std::ios::binary);
 
     if (fileStream.is_open() == false)
     {
@@ -117,10 +211,9 @@ int main()
 
     uintmax_t zipFileSize = std::filesystem::file_size(zipFilepath);
 
-    wchar_t* buffer = new wchar_t[zipFileSize + 0] { 0 };
+    uint8_t* zipFileBuffer = new uint8_t[zipFileSize + 0] { 0 };
 
-    fileStream.read(buffer, zipFileSize);
-
+    fileStream.read((char*)&zipFileBuffer[0], zipFileSize);
 
     std::vector<std::vector<Byte>> files;
 
@@ -133,34 +226,43 @@ int main()
 
     while (indexer < zipFileSize)
     {
-        pkSignature = buffer[indexer + 0];
+        pkSignature = zipFileBuffer[indexer + 0];
         pkSignature <<= 8;
-        pkSignature |= buffer[indexer + 1];
+        pkSignature |= zipFileBuffer[indexer + 1];
         pkSignature <<= 8;
-        pkSignature |= buffer[indexer + 2];
+        pkSignature |= zipFileBuffer[indexer + 2];
         pkSignature <<= 8;
-        pkSignature |= buffer[indexer + 3];
+        pkSignature |= zipFileBuffer[indexer + 3];
 
-        if (pkSignature == PK_SIGNATURE)
+        if (pkSignature == PK_FILE_HEADER_SIGNATURE)
         {
             files.push_back(std::vector<Byte>());
             fileIndexer++;
         };
 
-        files[fileIndexer].emplace_back(buffer[indexer]);
+        files[fileIndexer].emplace_back(zipFileBuffer[indexer]);
 
         indexer++;
     };
 
 
-    Byte comp1 = files[3][8];
-    Byte comp2 = files[3][9];
+    std::vector<Byte> endCentralDirectoryOut;
+    GetEndCentralDirectory(zipFileBuffer, zipFileSize, endCentralDirectoryOut);
 
-    short compressionMethod = comp1;
-    if (comp2 != 0)
+    int CentralDirectoryOffset = (endCentralDirectoryOut[16] |
+                                  endCentralDirectoryOut[17] << 8 |
+                                  endCentralDirectoryOut[18] << 16 |
+                                  endCentralDirectoryOut[19] << 24);
+
+    int sig = (zipFileBuffer[20276] |
+               zipFileBuffer[20277] << 8 |
+               zipFileBuffer[20278] << 16 |
+               zipFileBuffer[20279] << 24);
+
+
+    if (sig == PK_CENTRAL_DIRECTORY_SIGNATURE_LITTLE_ENDIAN)
     {
-        compressionMethod >>= 8;
-        compressionMethod |= comp2;
+        int s = 0;
     };
 
     for (int a = 0; a < files.size(); a++)
@@ -183,6 +285,6 @@ int main()
     };
 
 
-    delete[] buffer;
-    buffer = nullptr;
+    delete[] zipFileBuffer;
+    zipFileBuffer = nullptr;
 };
